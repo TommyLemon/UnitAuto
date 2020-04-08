1f01180bbc79f41447f6bb6b4112dd5b074c3945
TommyLemon <1184482681@qq.com>
2020-04-09 02:03:02 +0800

    Server：解决执行非 static 方法后 instance 返回 null；解决不传 type 也会强制补全，导致 classArgs 所有 type 都为 null 时仍然用了 value.getClass，从而导致 DemoFunction 等构造方法不传 type 始终报错；新增对比用的 JUnit 单元测试用例代码

--- UnitAuto-Backend/UnitAutoBoot/src/test/java/zuo/biao/apijson/server/ApplicationTests.java
+++ UnitAuto-Backend/UnitAutoBoot/src/test/java/zuo/biao/apijson/server/ApplicationTests.java
@@ -1,17 +1,46 @@
 package zuo.biao.apijson.server;
 
+import apijson.demo.server.MathUtil;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		//TODO Test Case，包括GitHub README介绍和简版demo
 	}
 
+	/**
+	 * FIXME 为了测试一个方法，参数值的范围往往很大，多个参数组合起来更是数量爆炸，根本没有时间精力去编码和测试！
+	 * 而 UnitAuto 只要配置
+	 * methodArgs/0/value : ORDER_INT(-10, 9990)
+	 * methodArgs/1/value : ORDER_INT(-9000, 1000)
+	 * 然后设置次数为 1000000000 再运行就会执行 1 亿次的各种组合情况，并且自动对比和展示结果！
+	 */
+	@Test
+	public void divide0() {
+		double a = 1;  //参数0，对应 UnitAuto methodArgs[0] = { "type": double, "value": 1 }
+		double b = 2;  //参数1，对应 UnitAuto methodArgs[1] = { "type": double, "value": 2 }
+		double c = 0.5;  //期望值，对应 UnitAuto 上传的结果 { "response": { "invoke": 0.5 ... } ... }
+		double d = MathUtil.divide(a, b);  //执行方法并返回实际值，对应 UnitAuto 执行的结果 { "invoke": 0.5, "instance": null ... }
+
+		Assert.assertEquals("1/2 != 0.5 !!!", c, d, 0);  //对比结果，对应 UnitAuto 自动对比和展示结果，红黄蓝绿
+	}
+
+	@Test
+	public void divide1() {
+		double a = 12;  //参数0，对应 UnitAuto methodArgs[0] = { "type": double, "value": 12 }
+		double b = -3;  //参数1，对应 UnitAuto methodArgs[1] = { "type": double, "value": -3 }
+		double c = -4;  //期望值，对应 UnitAuto 上传的正确结果 { "response": { "invoke": -4 ... } ... }
+		double d = MathUtil.divide(a, b);  //执行方法并返回实际值，对应 UnitAuto 执行的结果 { "invoke": -4, "instance": null ... }
+
+		Assert.assertEquals("12/-3 != -4 !!!", c, d, 0);  //对比结果，对应 UnitAuto 自动对比和展示结果，红黄蓝绿
+	}
+
+	//TODO divide2, divide3, ... 或者写数组 aList = [1, 12 ...], bList = [2, -3 ...], cList = [0.5, -4 ...] 然后遍历再调用
 }
