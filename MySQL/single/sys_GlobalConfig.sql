773092e8de4a52b02de273365a45cb581716b147
TommyLemon <1184482681@qq.com>
2022-04-11 01:25:31 +0800

    MySQL：更新 SQL 文件

--- MySQL/single/sys_GlobalConfig.sql
+++ MySQL/single/sys_GlobalConfig.sql
@@ -16,32 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Response`
+-- Table structure for table `GlobalConfig`
 --
 
-DROP TABLE IF EXISTS `Response`;
+DROP TABLE IF EXISTS `GlobalConfig`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Response` (
+CREATE TABLE `GlobalConfig` (
   `id` bigint(15) NOT NULL COMMENT '唯一标识',
-  `method` varchar(10) DEFAULT 'GET' COMMENT '方法',
-  `model` varchar(20) NOT NULL COMMENT '表名，table是SQL关键词不能用',
-  `structure` json NOT NULL COMMENT '结构',
-  `detail` varchar(10000) DEFAULT NULL COMMENT '详细说明',
-  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建日期',
+  `projectId` bigint(15) NOT NULL DEFAULT '0' COMMENT '项目 id',
+  `requestHeader` varchar(1000) DEFAULT NULL COMMENT '请求头',
+  `responseHeader` varchar(1000) DEFAULT NULL COMMENT '响应头',
+  `requestRandom` varchar(1000) DEFAULT NULL COMMENT '请求参数注入配置',
+  `responseRandom` varchar(1000) DEFAULT NULL COMMENT '响应参数注入配置',
+  `breforeScript` varchar(5000) DEFAULT NULL COMMENT '前置执行脚本，一般用于初始化数据',
+  `afterScript` varchar(5000) DEFAULT NULL COMMENT '后置执行脚本，一般用于断言和还原数据',
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='返回结果处理配置(目前未用上)\n每次启动服务器时加载整个表到内存。';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='全局配置  // TODO 是否改成把内容分散到原来的各个表，然后通过字段 is_global = 1 及 is_before = 1 等 标识？ 另外需要单独拆分出 Header 表，为了实现和 Random 一样与 Document 多对一，批量覆盖不同场景，也必须拆分出 Header';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Response`
+-- Dumping data for table `GlobalConfig`
 --
 
-LOCK TABLES `Response` WRITE;
-/*!40000 ALTER TABLE `Response` DISABLE KEYS */;
-INSERT INTO `Response` VALUES (1,'GET','User','{\"put\": {\"extra\": \"Response works! Test:He(She) is lazy and wrote nothing here\"}, \"remove\": \"phone\"}',NULL,'2017-05-22 12:36:47'),(2,'DELETE','Comment','{\"remove\": \"Comment:child\"}',NULL,'2017-05-03 17:51:26'),(3,'DELETE','Moment','{\"remove\": \"Comment\"}',NULL,'2017-05-03 17:51:26');
-/*!40000 ALTER TABLE `Response` ENABLE KEYS */;
+LOCK TABLES `GlobalConfig` WRITE;
+/*!40000 ALTER TABLE `GlobalConfig` DISABLE KEYS */;
+/*!40000 ALTER TABLE `GlobalConfig` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -53,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-20  4:57:50
+-- Dump completed on 2022-04-11  1:21:19
