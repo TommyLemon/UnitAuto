9b7200188a596a9502b5a4a591613f27f2ee4ffc
TommyLemon <tommylemon@qq.com>
2023-03-17 18:46:26 +0800

    Admin：解决有时导入报错；优化错误处理提示的体验

--- UnitAuto-Java-Demo/src/main/resources/static/js/main.js
+++ UnitAuto-Java-Demo/src/main/resources/static/js/main.js
@@ -212,23 +212,28 @@
                 if (JSONObject.isTableKey(firstKey, val, isRestful)) {
                   // var newVal = JSON.parse(JSON.stringify(val[i]))
 
-                  var newVal = {}
-                  for (var k in val[i]) {
-                    newVal[k] = val[i][k] //提升性能
-                    delete val[i][k]
+                  var vi = val[i]
+                  if (vi == null) {
+                    continue
                   }
 
-                  val[i]._$_this_$_ = JSON.stringify({
+                  var newVal = {}
+                  for (var k in vi) {
+                    newVal[k] = vi[k] //提升性能
+                    delete vi[k]
+                  }
+
+                  vi._$_this_$_ = JSON.stringify({
                     path: cPath + '/' + i,
                     table: firstKey
                   })
 
                   for (var k in newVal) {
-                    val[i][k] = newVal[k]
+                    vi[k] = newVal[k]
                   }
                 }
                 else {
-                  this.onRenderJSONItem(val[i], '' + i, cPath);
+                  this.onRenderJSONItem(vi, '' + i, cPath);
                 }
 
                 // this.$children[i]._$_this_$_ = key
@@ -265,7 +270,11 @@
 
 
         } catch (e) {
-          alert('onRenderJSONItem  try { ... } catch (e) {\n' + e.message)
+          if (DEBUG) {
+            alert('onRenderJSONItem  try { ... } catch (e) {\n' + e.message)
+          } else {
+            console.log(e)
+          }
         }
 
         return true
@@ -369,8 +378,8 @@
                 valString = valString.substring(0, i + 1)
                 // alert('valString = ' + valString)
                 var _$_this_$_ = JSON.parse(valString) || {}
-                path = _$_this_$_.path
-                table = _$_this_$_.table
+                path = _$_this_$_ == null ? '' : _$_this_$_.path
+                table = _$_this_$_ == null ? '' : _$_this_$_.table
               }
             }
 
@@ -454,10 +463,8 @@
 
 // APIJSON <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-  var ERR_MSG = `
-  
-可能出现了一些问题，可以按照以下步骤解决：
-1.检查网络连接是否畅通，可用浏览器打开右侧地址： https://www.baidu.com/s?wd=%22APIJSON%22 
+  var ERR_MSG = `出现了一些问题，可以按照以下步骤解决：
+1.检查网络连接是否畅通，可用浏览器打开右侧地址： https://www.baidu.com/s?wd=%22APIJSON%22
 2.检查 URL 是否为一个可用的 域名/IPV4 地址，可用浏览器打开试试：正常返回结果 或 非 GET 请求返回 Whitelabel Error Page，一般都没问题
 3.开启或关闭 右上方 设置>托管服务器代理，然后再试：如果开启后才通应该是 CORS 跨域问题；关闭后才通应该是用外网服务代理来访问内网导致，可退出登录后修改退关服务器地址为内网的 APIJSON 代理服务地址
 4.Disable 关闭 VPN 等 电脑/手机/平板 上的网络代理软件 App 客户端，或者切换代理服务器地址，然后再试
@@ -684,6 +691,7 @@ https://github.com/Tencent/APIJSON/issues
   var baseUrl
   var inputted
   var handler
+  var errHandler
   var docObj
   var doc
   var output
@@ -928,10 +936,10 @@ https://github.com/Tencent/APIJSON/issues
       branch: 'countArray',
       database: undefined,  // 后端决定 'MYSQL',// 'POSTGRESQL',
       schema: undefined,  // 后端决定 'admin',  // 'sys'
-      server: 'http://apijson.cn:9090',  //'http://apijson.cn:8080',  //apijson.cn
       otherEnv: 'http://localhost:8080',  // 其它环境服务地址，用来对比当前的
+      server: 'http://apijson.cn:9090',  //apijson.cn
       // server: 'http://47.74.39.68:9090',  // apijson.org
-      project: 'http://localhost:8081',  // 'http://apijson.cn:8080', //
+      project: 'http://localhost:8081', // 'http://apijson.cn:8080', // 
       language: 'Java,Kotlin,Go', // CodeUtil.LANGUAGE_JAVA,
       header: {},
       page: 0,
@@ -3149,6 +3157,11 @@ https://github.com/Tencent/APIJSON/issues
           }
           packagePrefix = packagePrefix.trim()
 
+          var langauges = StringUtil.split(this.language, ',', true)
+          if (langauges == null || langauges.length <= 0) {
+            langauges = undefined
+          }
+
           var req = {
             format: false,
             '[]': {
@@ -3162,6 +3175,7 @@ https://github.com/Tencent/APIJSON/issues
                 'defination()': 'getMethodDefination(method,arguments,type,exceptions,null)',
                 'constructorArguments()': 'getMethodArguments(genericClassArgs)',
                 'request()': 'getMethodRequest()',
+                'language{}': langauges,
                 'package$': StringUtil.isEmpty(packagePrefix) ? null : packagePrefix + '%',
                 'class$': StringUtil.isEmpty(classPrefix) ? null : classPrefix + '%',
                 'package*~': search,
@@ -3740,7 +3754,7 @@ https://github.com/Tencent/APIJSON/issues
 
                   App.onResponse(url_, res_, err_);
                   App.onLoginResponse(isAdminOperation, req, url, res, err)
-            }, this.scripts)
+            }, App.scripts)
 
           })
         }
@@ -4392,7 +4406,12 @@ https://github.com/Tencent/APIJSON/issues
           "method": this.getMethod(url)
         }, req)
 
-        vOutput.value = "requesting... \nURL = " + url + ERR_MSG
+        vOutput.value = "requesting... \nURL = " + url
+
+        errHandler = function () {
+          vOutput.value = "requesting... \nURL = " + url + "\n\n可能" + ERR_MSG
+        }
+        setTimeout(errHandler, 5000)
 
         this.view = 'output';
 
@@ -4455,6 +4474,7 @@ https://github.com/Tencent/APIJSON/issues
             // crossDomain: true
           })
             .then(function (res) {
+              clearTimeout(errHandler)
               var postEvalResult = evalPostScript(url, res, null)
               if (postEvalResult == BREAK_ALL) {
                 return
@@ -4755,7 +4775,7 @@ https://github.com/Tencent/APIJSON/issues
             // vOutput.value = "Response:\nurl = " + url + "\nerror = " + err.message;
             this.view = 'error';
             this.error = {
-              msg: "Response:\nurl = " + url + "\nerror = " + err.message + ERR_MSG
+              msg: "Response:\nurl = " + url + "\nerror = " + err.message + '\n\n' + ERR_MSG
             }
           }
         }
